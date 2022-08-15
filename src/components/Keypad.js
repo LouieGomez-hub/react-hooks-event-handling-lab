5e61598595b2d8d175072cf458d15b0c238e192f
louie_gomez <kinggomez027.lg@gmail.com>
2022-08-15 15:11:48 -0400

    wrote event handlers, completed lab

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,8 +1,13 @@
-// Code Keypad Component Here
+import React from "react"
+<Keypad />
 
 function Keypad (){
     return (
-        <div></div>
+        <div>
+            <input 
+                type="password"
+                onChange={() => console.log("Entering password...")} />
+        </div>
     )
 }
 
